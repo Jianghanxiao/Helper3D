63051fc475924061d055be0bc9c3d7c4264409f9
HanxiaoJiang <shawn_jiang@sfu.ca>
2020-08-26 23:21:35 -0700

    finish joint parser; todo: deal with multiple visual in link

--- src/urdf/Link.py
+++ src/urdf/Link.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Link:
     def __init__(self, link_name):
         self.link_name = link_name
@@ -10,22 +11,21 @@ class Link:
 
     def setVisualOriginXyz(self, xyz):
         self.has_visual = True
-        self.visual_origin['xyz'] = np.array(xyz)
+        self.visual_origin["xyz"] = np.array(xyz)
 
     def setVisualOriginRpy(self, rpy):
         self.has_visual = True
-        self.visual_origin['rpy'] = np.array(rpy)
+        self.visual_origin["rpy"] = np.array(rpy)
 
     def setVisualGeometryMeshFilename(self, filename):
         self.has_visual = True
-        self.visual_geometry_mesh['filename'] = filename
+        self.visual_geometry_mesh["filename"] = filename
 
     def __repr__(self):
         output = {}
-        output['name'] = self.link_name
-        output['has_visual'] = self.has_visual
-        output['xyz'] = self.visual_origin['xyz']
-        output['rpy'] = self.visual_origin['rpy']
-        output['filename'] = self.visual_geometry_mesh['filename']
+        output["name"] = self.link_name
+        output["has_visual"] = self.has_visual
+        output["origin"] = self.visual_origin
+        output["filename"] = self.visual_geometry_mesh["filename"]
 
         return str(output)
--- src/urdf/URDFParser.py
+++ src/urdf/URDFParser.py
@@ -1,16 +1,20 @@
 import xml.etree.ElementTree as ET
 import numpy as np
 from Link import Link
+from Joint import Joint
+
 
 def parseThreeNumber(string):
-    strings = string.split(' ')
+    strings = string.split(" ")
     numbers = np.array(list(map(float, strings)))
     return numbers
+
 
 class URDFParser:
     def __init__(self, file_name):
         self.file_name = file_name
         self.links = {}
+        self.joints = {}
 
     # Parse the URDF(XML) file into a tree structure
     def parse(self):
@@ -24,31 +28,60 @@ class URDFParser:
 
     def parseLinks(self):
         for link_xml in self.links_xml:
-            link_name = link_xml.attrib['name']
+            link_name = link_xml.attrib["name"]
             link = Link(link_name)
             # Deal with the visual part
             visual_xml = link_xml.find("visual")
-            if(visual_xml != None):
+            if visual_xml != None:
                 # Get origin
-                origin_xml = visual_xml.find('origin')
-                if(origin_xml != None):
-                    if('xyz' in origin_xml.attrib):
-                        xyz = parseThreeNumber(origin_xml.attrib['xyz'])
+                origin_xml = visual_xml.find("origin")
+                if origin_xml != None:
+                    if "xyz" in origin_xml.attrib:
+                        xyz = parseThreeNumber(origin_xml.attrib["xyz"])
                         link.setVisualOriginXyz(xyz)
-                    if('rpy' in origin_xml.attrib):
-                        rpy = parseThreeNumber(origin_xml.attrib['rpy'])
+                    if "rpy" in origin_xml.attrib:
+                        rpy = parseThreeNumber(origin_xml.attrib["rpy"])
                         link.setVisualOriginRpy(rpy)
                 # Get geometry
-                geometry_xml = visual_xml.find('geometry')
-                if(geometry_xml != None):
-                    mesh_xml = geometry_xml.find('mesh')
-                    if(mesh_xml != None):
-                        filename = mesh_xml.attrib['filename']
+                geometry_xml = visual_xml.find("geometry")
+                if geometry_xml != None:
+                    mesh_xml = geometry_xml.find("mesh")
+                    if mesh_xml != None:
+                        filename = mesh_xml.attrib["filename"]
                         link.setVisualGeometryMeshFilename(filename)
             self.links[link_name] = link
 
     def parseJoints(self):
-        pass
+        for joint_xml in self.joints_xml:
+            joint_name = joint_xml.attrib["name"]
+            joint_type = joint_xml.attrib["type"]
+            child_name = joint_xml.find("child").attrib["link"]
+            parent_name = joint_xml.find("parent").attrib["link"]
+            joint = Joint(
+                joint_name, joint_type, self.links[child_name], self.links[parent_name]
+            )
+            # Get origin
+            origin_xml = joint_xml.find("origin")
+            if origin_xml != None:
+                if "xyz" in origin_xml.attrib:
+                    xyz = parseThreeNumber(origin_xml.attrib["xyz"])
+                    joint.setOriginXyz(xyz)
+                if "rpy" in origin_xml.attrib:
+                    rpy = parseThreeNumber(origin_xml.attrib["rpy"])
+                    joint.setOriginRpy(rpy)
+            # Get Axis
+            axis_xml = joint_xml.find("axis")
+            if axis_xml != None:
+                axis = parseThreeNumber(axis_xml.attrib["xyz"])
+                joint.setAxis(axis)
+            # Get Limit
+            limit_xml = joint_xml.find("limit")
+            if limit_xml != None:
+                lower = float(limit_xml.attrib["lower"])
+                upper = float(limit_xml.attrib["upper"])
+                joint.setLimitLower(lower)
+                joint.setLimitUpper(upper)
+            self.joints[joint_name] = joint
 
 
 if __name__ == "__main__":
@@ -56,6 +89,5 @@ if __name__ == "__main__":
     parser = URDFParser(file_name)
     parser.parse()
 
-    for i in parser.links:
-        print(parser.links[i])
-
+    for i in parser.joints:
+        print(parser.joints[i])
