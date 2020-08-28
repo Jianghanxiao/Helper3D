f7b43498a3b35456da3573a9bec6235acccfcef2
HanxiaoJiang <shawn_jiang@sfu.ca>
2020-08-28 14:10:53 -0700

    finish basic transform and meshnode; todo: deal with draw logic and rotate, translate for the scenenode

--- src/SceneGraph/MeshNode.py
+++ src/SceneGraph/MeshNode.py
@@ -1,4 +1,16 @@
+import open3d as o3d
 class MeshNode:
-    def __init__(self, mesh=None, mesh_file=None):
+    def __init__(self, mesh = None):
         self.mesh = mesh
-        self.mesh_file = mesh_file
+
+    def addMesh(self, mesh):
+        if self.mesh == None:
+            self.mesh = mesh
+        else:
+            self.mesh += mesh
+        self.mesh.remove_unreferenced_vertices()
+
+    def addMeshFile(self, mesh_file):
+        # Read the mesh from obj file
+        mesh = o3d.io.read_triangle_mesh(mesh_file)
+        self.addMesh(mesh)
--- src/SceneGraph/SceneNode.py
+++ src/SceneGraph/SceneNode.py
@@ -1,12 +1,37 @@
 from .Transform import Transform
+from .MeshNode import MeshNode
 import numpy as np
 
 class SceneNode:
-    def __init__(self):
-         self.parent = None
-         self.children = []
-         # Store the local transform and world transform
-         self.localTransform = Transform()
-         self.worldMatrix = np.identity(4)
-         # Store the mesh and deal with functions draw the mesh based on the transform
-         self.meshNode = None
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.children = []
+        # Store the local transform and world transform
+        self.localTransform = Transform()
+        if parent == None:
+            self.worldMatrix = self.localTransform.getMatrix()
+        else:
+            self.worldMatrix = np.dot(self.parent.worldMatrix, self.localTransform.getMatrix())    
+        # Store the mesh and deal with functions draw the mesh based on the transform
+        self.meshNode = MeshNode()
+
+    def setParent(self, parent):
+        if parent == None:
+            raise RuntimeError('Invalid Parent: parent is not in the SceneNode type')
+        self.parent = parent
+        self.worldMatrix = np.dot(self.parent.worldMatrix, self.localTransform.getMatrix()) 
+
+    def update(self):
+        # Update the worldMatrix of current scene node
+        self.worldMatrix = np.dot(self.parent.worldMatrix, self.localTransform.getMatrix())    
+    
+    def addChild(self, child):
+        # child should also be SceneNode
+        self.children.append(child)
+
+    def addMesh(self, mesh):
+        # mesh should be in open3d form
+        self.meshNode.addMesh(mesh)
+    
+    def addMeshFile(self, mesh_file):
+        self.meshNode.addMeshFile(mesh_file)
