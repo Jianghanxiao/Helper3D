5129061edde7cb755110d96a32f2f411c01ba281
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2023-03-19 15:30:29 -0700

    fixing a small problem when using the copy() function in trimesh.Scene, their geometry copy may have some problem

--- trimesh_URDF/src/SceneGraph/MeshNode.py
+++ trimesh_URDF/src/SceneGraph/MeshNode.py
@@ -1,5 +1,34 @@
 import trimesh
 
+# This function doesn't copy the geometry. Modify based on https://github.com/mikedh/trimesh/blob/main/trimesh/scene/scene.py#L1194
+# The original function has something wrong with the geometry copy
+def scene_transform_copy(mesh):
+    """
+    Return a deep copy of the current scene
+    Returns
+    ----------
+    copied : trimesh.Scene
+        Copy of the current scene
+    """
+    # Doesn't copy the geometry
+    geometry = mesh.geometry
+
+    if not hasattr(mesh, "_camera") or mesh._camera is None:
+        # if no camera set don't include it
+        camera = None
+    else:
+        # otherwise get a copy of the camera
+        camera = mesh.camera.copy()
+    # create a new scene with copied geometry and graph
+    copied = trimesh.Scene(
+        geometry=geometry,
+        graph=mesh.graph.copy(),
+        metadata=mesh.metadata.copy(),
+        camera=camera,
+    )
+    return copied
+
+
 class MeshNode:
     def __init__(self):
         self.mesh = None
@@ -21,7 +50,7 @@ class MeshNode:
 
     def getMesh(self, worldMatrix):
         if self.mesh == None:
-            return None    
-        new_mesh = self.mesh.copy()
+            return None
+        new_mesh = scene_transform_copy(self.mesh)
         new_mesh.apply_transform(worldMatrix)
         return new_mesh
