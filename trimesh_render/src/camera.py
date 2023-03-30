3dfac8d8ad4feec2966dfac9a338233bff2f39ac
Hanxiao Jiang <shawn.x.jiang@gmail.com>
2023-03-30 13:39:22 -0700

    fix lookAt function

--- trimesh_render/src/camera.py
+++ trimesh_render/src/camera.py
@@ -8,12 +8,12 @@ def lookAt(eye, target, up):
     forward /= np.linalg.norm(forward)
     if np.dot(forward, up) == 1 or np.dot(forward, up) == -1:
         up = np.array([0.0, 1.0, 0.0])
-    right = np.cross(forward, up)
+    right = np.cross(up, forward)
     right /= np.linalg.norm(right)
-    new_up = np.cross(right, forward)
+    new_up = np.cross(forward, right)
     new_up /= np.linalg.norm(new_up)
 
-    # Construct a rotation matrix from the right, new_up, and -forward vectors
+    # Construct a rotation matrix from the right, new_up, and forward vectors
     rotation = np.eye(4)
     rotation[:3, :3] = np.row_stack((right, new_up, forward))
 
@@ -26,8 +26,5 @@ def lookAt(eye, target, up):
     ]
 
     camera_pose = np.linalg.inv(np.matmul(translation, rotation))
-
-    # The above transformation is left-handed, but trimesh or pyrender uses right-handed
-    camera_pose[1, :] *= -1
 
     return camera_pose
